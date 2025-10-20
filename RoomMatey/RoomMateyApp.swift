a549939aa3c908e890ff8413c00438cd1978dea0
Ian Quack <ian.quack@icloud.com>
2025-10-20 11:38:28 -0400

    Initial Commit

---
+++ RoomMatey/RoomMateyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomMateyApp.swift
+//  RoomMatey
+//
+//  Created by Ian Quack on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RoomMateyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
